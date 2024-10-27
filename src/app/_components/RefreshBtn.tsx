1c7e87c22f3c53139d44ac4cc5ce04e954ccc256
chaechae66 <valueworkerchae@gmail.com>
2024-10-27 17:31:12 +0900

    Refactor: next에서 캐싱된 데이터 리프래쉬 시키는 버튼 구현

---
+++ src/app/_components/RefreshBtn.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import action from "../actions/action";
+
+interface Props {
+  path: string;
+}
+
+export default function RefreshBtn({ path }: Props) {
+  const handleRefresh = async () => {
+    await action(path);
+  };
+  return <button onClick={handleRefresh}>새로고침</button>;
+}
--- src/app/user/[username]/default/page.tsx
+++ src/app/user/[username]/default/page.tsx
@@ -1,81 +1,113 @@
+import RefreshBtn from "@/app/_components/RefreshBtn";
+
 interface TCharacterDefault {
-  "character_name": string,
-  "world_name": string,
-  "character_gender": string,
-  "character_class": string,
-  "character_class_level": string,
-  "character_level": number,
-  "character_exp": number,
-  "character_exp_rate": string,
-  "character_guild_name": string | null,
-  "character_image": string,
-  "popularity": 0,
-  "final_stat": 
-      {
-          "stat_name": string,
-          "stat_value": string
-      }[]
+  result: {
+    character_name: string;
+    world_name: string;
+    character_gender: string;
+    character_class: string;
+    character_class_level: string;
+    character_level: number;
+    character_exp: number;
+    character_exp_rate: string;
+    character_guild_name: string | null;
+    character_image: string;
+    popularity: 0;
+    final_stat: {
+      stat_name: string;
+      stat_value: string;
+    }[];
+  };
 }
 
-export default async function Page({ params }: { params: { username: string } }) {
-  const characterData = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/character/info?character_name=${decodeURIComponent(params.username)}`, { next: { tags: ["basic"] } })
+export default async function Page({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const characterData = await fetch(
+    `${
+      process.env.NEXT_PUBLIC_BASE_URL
+    }/character/info?character_name=${decodeURIComponent(params.username)}`
+  );
 
-  const jsonData : TCharacterDefault = await characterData.json();
-  const statsObject = jsonData.final_stat.reduce<Record<string, string>>((acc, stat) => {
-    acc[stat.stat_name] = stat.stat_value;
-    return acc;
-}, {});
+  const { result }: TCharacterDefault = await characterData.json();
+  const statsObject = result.final_stat?.reduce<Record<string, string>>(
+    (acc, stat) => {
+      acc[stat.stat_name] = stat.stat_value;
+      return acc;
+    },
+    {}
+  );
 
+  return (
+    <>
+      <div>
+        <RefreshBtn
+          path={`/character/info?character_name=${decodeURIComponent(
+            params.username
+          )}`}
+        />
+        <h3 className="text-xl">프로필</h3>
+        <hr />
+        <div className="flex">
+          <img
+            src={result.character_image}
+            alt={result.character_name}
+            className="w-32 h-42 m-8"
+          />
+          <div className="flex flex-col py-4 items-start">
+            <div className="none-seleceted-btn">{result.world_name}</div>
+            <div className="mt-2">
+              <span className="text-2xl mr-2">{result.character_name}</span>
+              <span className="mr-1">Lv.{result.character_level}</span>
+              <span>{result.character_gender}</span>
+            </div>
+            <div className="mt-2">
+              <p>
+                {`Class Lv.${result.character_class_level} ${result.character_class}`}
+                <br />
+                {`인기도 : ${result.popularity}`}
+                <br />
+                {`길드 명 : ${
+                  result.character_guild_name
+                    ? result.character_guild_name
+                    : "없음"
+                }`}
+                <br />
+                {`경험치 : ${result.character_exp}  경험치 비율 : ${result.character_exp_rate}`}
+              </p>
+            </div>
+          </div>
+        </div>
 
-  
-  return <>
-    <div>
-      <h3 className="text-xl">프로필</h3>
-      <hr />
-      <div className="flex">
-      <img src={jsonData.character_image} alt={jsonData.character_name} className="w-32 h-42 m-8" />
-      <div className="flex flex-col py-4 items-start">
-        <div className="none-seleceted-btn">{jsonData.world_name}</div>
-        <div className="mt-2"><span className="text-2xl mr-2">{jsonData.character_name}</span><span className="mr-1">Lv.{jsonData.character_level}</span><span>{jsonData.character_gender}</span></div>
-        <div className="mt-2">
-        <p>
-         { `Class Lv.${jsonData.character_class_level} ${jsonData.character_class}`}<br />
-         {`인기도 : ${jsonData.popularity}`}
-         <br />
-         {`길드 명 : ${jsonData.character_guild_name ? jsonData.character_guild_name : "없음" }`}
-         <br />
-         {`경험치 : ${jsonData.character_exp}  경험치 비율 : ${jsonData.character_exp_rate}`}
-        </p>
+        <h3 className="text-xl mt-4">스탯</h3>
+        <hr />
+        <table className="w-full border-b-[1px] border-gray-500 border-solid">
+          <tbody>
+            <tr>
+              <td className="pb-2 pl-2">{`${statsObject["최소 스탯공격력"]} ~ ${statsObject["최대 스탯공격력"]}`}</td>
+              <td className="pb-2">{statsObject.HP}</td>
+              <td className="pb-2">{statsObject.MP}</td>
+              <td className="pb-2">{statsObject.STR}</td>
+              <td className="pb-2">{statsObject.DEX}</td>
+              <td className="pb-2">{statsObject.INT}</td>
+              <td className="pb-2">{statsObject.LUK}</td>
+            </tr>
+          </tbody>
+          <thead>
+            <tr className="text-gray-400">
+              <td className="pt-2 pl-2">스탯 공격력</td>
+              <td className="pt-2">HP</td>
+              <td className="pt-2">MP</td>
+              <td className="pt-2">STR</td>
+              <td className="pt-2">DEX</td>
+              <td className="pt-2">INT</td>
+              <td className="pt-2">LUK</td>
+            </tr>
+          </thead>
+        </table>
       </div>
-      </div>
-      </div>
-
-      <h3 className="text-xl mt-4">스탯</h3>
-      <hr />
-      <table className="w-full border-b-[1px] border-gray-500 border-solid">
-        <tbody>
-        <tr>
-          <td className="pb-2 pl-2">{`${statsObject["최소 스탯공격력"]} ~ ${statsObject["최대 스탯공격력"]}`}</td>
-          <td className="pb-2">{statsObject.HP}</td>
-          <td className="pb-2">{statsObject.MP}</td>
-          <td className="pb-2">{statsObject.STR}</td>
-          <td className="pb-2">{statsObject.DEX}</td>
-          <td className="pb-2">{statsObject.INT}</td>
-          <td className="pb-2">{statsObject.LUK}</td>
-        </tr>
-        </tbody>
-        <thead>
-        <tr className="text-gray-400">
-          <td className="pt-2 pl-2">스탯 공격력</td>
-          <td className="pt-2">HP</td>
-          <td className="pt-2">MP</td>
-          <td className="pt-2">STR</td>
-          <td className="pt-2">DEX</td>
-          <td className="pt-2">INT</td>
-          <td className="pt-2">LUK</td>
-        </tr>
-        </thead>
-      </table>
-    </div>
-  </>
+    </>
+  );
 }
