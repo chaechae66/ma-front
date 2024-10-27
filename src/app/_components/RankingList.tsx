c9d2b5dd358d73f4e78127d8afb627da8a058334
chaechae66 <valueworkerchae@gmail.com>
2024-10-27 17:31:59 +0900

    Chore: 백엔드 데이터 포멧 변경으로 인해 수정

--- src/app/_components/RankingList.tsx
+++ src/app/_components/RankingList.tsx
@@ -1,29 +1,37 @@
 interface Props<T> {
-  tableTitles : {title: string, key: keyof T}[]
-  data : T[]
+  tableTitles: { title: string; key: keyof T }[];
+  data: T[];
 }
 
-export default function RankingList<T extends { ranking: number }>({tableTitles, data} :Props<T>) {
-  return <>
-  
-    <table className="w-full text-center">
-    <thead>
-                <tr className="bg-zinc-600 h-12 border-t-[1px] border-b-[1px] border-zinc-500">
-                 {tableTitles.map((elem)=>(
-                   <td key={elem.title}>{elem.title}</td>
-                 ))}
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((elem)=>(
-                  <tr className="h-12 border-b-[1px] border-zinc-500" key={elem.ranking}>{
-                    tableTitles.map((value)=>(
-                    <td key={value.title}>
-                      {(elem[value.key]  as React.ReactNode) || "-"}
-                      </td>))
-                  }</tr>
-                ))}
-              </tbody>
-    </table>
-  </>;
+export default function RankingList<T extends { ranking: number }>({
+  tableTitles,
+  data,
+}: Props<T>) {
+  return (
+    <>
+      <table className="w-full text-center">
+        <thead>
+          <tr className="bg-zinc-600 h-12 border-t-[1px] border-b-[1px] border-zinc-500">
+            {tableTitles.map((elem) => (
+              <td key={elem.title}>{elem.title}</td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((elem) => (
+            <tr
+              className="h-12 border-b-[1px] border-zinc-500"
+              key={elem.ranking}
+            >
+              {tableTitles.map((value) => (
+                <td key={value.title}>
+                  {(elem[value.key] as React.ReactNode) || "-"}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
