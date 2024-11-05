bff60efa8df3cee53e8e32b107c00adaba0a359d
chaechae66 <valueworkerchae@gmail.com>
2024-11-05 22:33:05 +0900

    Refactor: 날짜 10 미만일 때 "0" 붙여주기

--- src/app/_utils/calculateDay.ts
+++ src/app/_utils/calculateDay.ts
@@ -1,5 +1,7 @@
 export const calToday = () => {
   const date = new Date();
   date.setDate(date.getDate() - 1);
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${
+    date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
+  }`;
 };
