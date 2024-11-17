a2b78656fab55b993b8817d1a494f5031fef18dd
chaechae66 <valueworkerchae@gmail.com>
2024-11-18 08:44:19 +0900

    Chore: react-query staleTime, gcTime 설정

--- src/app/_components/Provider.tsx
+++ src/app/_components/Provider.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface Props {
   children: React.ReactNode;
 }
 
+const HOURS = 1000 * 60 * 60;
+
 export default function Provider({ children }: Props) {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 18 * HOURS,
+        gcTime: 24 * HOURS,
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
